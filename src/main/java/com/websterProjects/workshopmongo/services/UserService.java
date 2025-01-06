68d45a0dca41555f403c4905acbc742d385f2bb9
Webster Diogenes <web.driw12@gmail.com>
2025-01-06 17:08:48 -0300

    User insertion

--- src/main/java/com/websterProjects/workshopmongo/services/UserService.java
+++ src/main/java/com/websterProjects/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.websterProjects.workshopmongo.domain.User;
+import com.websterProjects.workshopmongo.dto.UserDTO;
 import com.websterProjects.workshopmongo.repository.UserRepository;
 import com.websterProjects.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,14 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	
+	public User insert (User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
 
 }
