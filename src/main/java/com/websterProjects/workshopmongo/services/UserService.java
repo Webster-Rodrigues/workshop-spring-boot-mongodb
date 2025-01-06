0dd7979a6d3c7f8a2e7fa751c2ca166b77d5b69c
Webster Diogenes <web.driw12@gmail.com>
2025-01-06 16:38:42 -0300

    Getting a user by ID

--- src/main/java/com/websterProjects/workshopmongo/services/UserService.java
+++ src/main/java/com/websterProjects/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.websterProjects.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.websterProjects.workshopmongo.domain.User;
 import com.websterProjects.workshopmongo.repository.UserRepository;
+import com.websterProjects.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,10 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 
 }
