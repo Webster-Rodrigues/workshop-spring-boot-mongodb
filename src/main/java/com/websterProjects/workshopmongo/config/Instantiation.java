a44df5d9377cf134c3b1e943e64443340488ca54
Webster Diogenes <web.driw12@gmail.com>
2025-01-13 17:37:39 -0300

    Add comments to posts

--- src/main/java/com/websterProjects/workshopmongo/config/Instantiation.java
+++ src/main/java/com/websterProjects/workshopmongo/config/Instantiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.websterProjects.workshopmongo.domain.Post;
 import com.websterProjects.workshopmongo.domain.User;
 import com.websterProjects.workshopmongo.dto.AuthorDTO;
+import com.websterProjects.workshopmongo.dto.CommentDTO;
 import com.websterProjects.workshopmongo.repository.PostRepository;
 import com.websterProjects.workshopmongo.repository.UserRepository;
 
@@ -29,8 +30,8 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
-		sfd.setTimeZone(TimeZone.getTimeZone("GMT"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
 		postRepository.deleteAll();
@@ -41,8 +42,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sfd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
-		Post post2 = new Post(null, sfd.parse("23/03/2020"), "Bom dia!", "Acordei bem", new AuthorDTO(alex));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia!", "Acordei bem", new AuthorDTO(alex));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c2 = new CommentDTO("Aproveita!", sdf.parse("22/03/2018"), new AuthorDTO(alex));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("22/03/2018"), new AuthorDTO(maria));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
